10a35293180d84e90fed93cc1c3180f6c81ebacd
quentinlimm <lim.quentin98@gmail.com>
2020-12-10 06:18:03 +0100

    commit

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
+const mongoose = require('mongoose');
+const expressEjsLayout = require('express-ejs-layouts')
+
+//mongoose
+mongoose.connect('mongodb+srv://quentin:quentin@devops.dzjg9.mongodb.net/<dbname>?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log('connected,,'))
+.catch((err)=> console.log(err));
+//EJS
+app.set('view engine','ejs');
+app.use(expressEjsLayout);
+//BodyParser
+app.use(express.urlencoded({extended : false}));
+
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
+
+app.listen(3000); 
